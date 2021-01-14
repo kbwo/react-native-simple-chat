d2a3bcb161588ca1ff52d77e0650139f7faf2690
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-01-15 00:12:52 +0900

    feat: make bootstrap

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import SimpleChat from 'react-native-simple-chat';
 
-
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
