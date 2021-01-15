8e4b78f0cf3255f10bbdd79e7f49e1836b05eadf
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-01-15 22:20:40 +0900

    feat: remove comment

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,3 @@
 import SimpleChat from './SimpleChat';
 
-// export default {
-//   multiply(a: number, b: number) {
-//     return Promise.resolve(a * b);
-//   },
-// };
 export default SimpleChat;
